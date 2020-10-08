550d79f197076283bba1ad6f3414c45240663adf
Ana Espinoza <aespinozaromero1@gmail.com>
2020-10-07 19:46:03 -0500

    commit main

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function names "fight"
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+fight ();
+
+window.prompt("What is your robot's name?");
